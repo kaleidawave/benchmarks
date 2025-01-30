5bf06a1d8e5bf2155bcfc33c77fcd72a9ee07cf5
Ben <kaleidawave@gmail.com>
2025-01-30 22:51:18 +0000

    MV fix

--- benchmarks/html-parsing/run.sh
+++ benchmarks/html-parsing/run.sh
@@ -1,13 +1,15 @@
 echo "::group::Setup"
 gh release download -R denoland/deno -p "deno-x86_64-unknown-linux-gnu.*"
 unzip deno-x86_64-unknown-linux-gnu.zip
-mv deno-x86_64-unknown-linux-gnu deno
+mv deno-x86_64-unknown-linux-gnu/deno deno
 pwd >> "$GITHUB_PATH"
 deno install
 curl https://www.bbc.co.uk/news > corpus.html
 echo "::endgroup::"
 
-echo "### Performance output" >> $GITHUB_STEP_SUMMARY
-echo "\`\`\`shell">> $GITHUB_STEP_SUMMARY
-deno bench comparison.ts >> $GITHUB_STEP_SUMMARY
-echo "\`\`\`">> $GITHUB_STEP_SUMMARY
+output="### Performance output
+
+\`\`\`shell
+$(deno bench comparison.ts)
+\`\`\`"
+echo output >> $GITHUB_STEP_SUMMARY
