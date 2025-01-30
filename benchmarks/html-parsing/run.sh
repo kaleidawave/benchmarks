1d5246120297a82df6b0a66998bc38d0a18aa51c
Ben <kaleidawave@gmail.com>
2025-01-30 22:53:25 +0000

    Add LS

--- benchmarks/html-parsing/run.sh
+++ benchmarks/html-parsing/run.sh
@@ -1,15 +1,18 @@
 echo "::group::Setup"
+
 gh release download -R denoland/deno -p "deno-x86_64-unknown-linux-gnu.*"
+ls
 unzip deno-x86_64-unknown-linux-gnu.zip
 mv deno-x86_64-unknown-linux-gnu/deno deno
 pwd >> "$GITHUB_PATH"
-deno install
+./deno install
 curl https://www.bbc.co.uk/news > corpus.html
+
 echo "::endgroup::"
 
 output="### Performance output
 
 \`\`\`shell
-$(deno bench comparison.ts)
+$(./deno bench comparison.ts)
 \`\`\`"
 echo output >> $GITHUB_STEP_SUMMARY
