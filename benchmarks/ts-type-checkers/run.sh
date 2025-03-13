683728314ff76a1750892593be4c5fe67593aa6f
Ben <kaleidawave@gmail.com>
2025-03-13 19:57:04 +0000

    Include tsc-go

--- benchmarks/ts-type-checkers/run.sh
+++ benchmarks/ts-type-checkers/run.sh
@@ -1,8 +1,17 @@
 NO_COLOR=1
 export NO_COLOR=1
 
-echo $ARTIFACTS_FOLDER
-echo $SOMETHING
+echo "## Example file `demo.ts`
+
+<details>
+<summary>Output</summary>
+
+\`\`\`
+$(cat demo.ts)
+\`\`\`
+</details>
+
+" >> $GITHUB_STEP_SUMMARY
 
 echo "::group::Run tools"
 
@@ -21,24 +30,83 @@ $OUTPUT
 " >> $GITHUB_STEP_SUMMARY
 } 
 
-run_tool "Ezno" "./ezno/target/release/ezno check demo.tsx --timings"
-run_tool "Ezno (next)" "./ezno-next/target/release/ezno check demo.tsx --timings"
-run_tool "TSC" "tsc --pretty --noEmit --skipLibCheck --jsx preserve --diagnostics demo.tsx"
-# run_tool "STC" "./stc/target/release/stc test demo.tsx"
+run_tool "Ezno" "./ezno/target/release/ezno check demo.ts --timings"
+run_tool "TSC" "./tsc-go/built/local/tsgo tsc -pretty -noEmit -skipLibCheck demo.ts"
 
 echo "::endgroup::"
+
+# ---
 
 echo "::group::Run benchmarks"
 
 echo "## Benchmark files" >> $GITHUB_STEP_SUMMARY
 
+./tsc-go/built/local/tsgo --help
+./tsc-go/built/local/tsgo tsc --help
+
+hyperfine -i \
+  './ezno/target/release/ezno check ./demo.ts' \
+  './tsc-go/built/local/tsgo tsc -skipLibCheck ./demo.ts' \
+  'tsc --pretty --skipLibCheck --noEmit --jsx preserve ./demo.ts'
+
+hyperfine -i \
+  './ezno/target/release/ezno check ./large.ts' \
+  './tsc-go/built/local/tsgo tsc -skipLibCheck ./large.ts' \
+  'tsc --pretty --skipLibCheck --noEmit --jsx preserve ./large.ts'
+
 # Ezno and TSC
-echo "\`\`\`
+echo "##### `demo.ts`
+
+\`\`\`
 $(hyperfine -i \
-  './ezno/target/release/ezno check ./demo.tsx' \
-  './ezno-next/target/release/ezno check ./demo.tsx' \
-  'tsc --pretty --skipLibCheck --noEmit --jsx preserve ./demo.tsx' \
+  './ezno/target/release/ezno check ./demo.ts' \
+  './tsc-go/built/local/tsgo tsc -skipLibCheck ./demo.ts' \
+  'tsc --pretty --skipLibCheck --noEmit --jsx preserve ./demo.ts'
 )
-\`\`\`" >> $GITHUB_STEP_SUMMARY
+\`\`\`
+
+##### `large.ts`
+
+\`\`\`
+$(hyperfine -i \
+  './ezno/target/release/ezno check ./large.ts' \
+  './tsc-go/built/local/tsgo tsc -skipLibCheck ./large.ts' \
+  'tsc --pretty --skipLibCheck --noEmit --jsx preserve ./large.ts'
+)
+\`\`\`
+" >> $GITHUB_STEP_SUMMARY
+
+### Valgrind
+valgrind --log-file="ezno-mem.txt" ./ezno/target/release/ezno check ./demo.ts
+valgrind --log-file="tsc-go-mem.txt" ./tsc-go/built/local/tsgo tsc -skipLibCheck ./demo.ts
+valgrind --log-file="tsc-mem.txt" tsc --pretty --skipLibCheck --noEmit --jsx preserve ./demo.ts
+
+echo "## Memory usage
+
+<details>
+<summary>ezno memory</summary>
+
+\`\`\`
+$(cat ezno-mem.txt)
+\`\`\`
+</details>
+
+<details>
+<summary>tsc-go memory</summary>
+
+\`\`\`
+$(cat tsc-go-mem.txt)
+\`\`\`
+</details>
+
+<details>
+<summary>tsc memory</summary>
+
+\`\`\`
+$(cat tsc-mem.txt)
+\`\`\`
+</details>
+
+" >> $GITHUB_STEP_SUMMARY
 
 echo "::endgroup::"
--- benchmarks/ts-type-checkers/setup.sh
+++ benchmarks/ts-type-checkers/setup.sh
@@ -1,58 +1,110 @@
+echo "::group::Get tools"
 # Install hyperfine
 brew install hyperfine
+sudo apt-get install valgrind
 
-echo "::group::Get tools"
+echo "::endgroup::"
 
-# TODO flow, hegel, install tools (or cache better), more
+# ---
 
-# echo "::group::Build STC"
-
-# rustup toolchain install nightly-2023-06-20
-
-# git clone https://github.com/dudykr/stc stc
-# cd stc
-# git reset --hard 693cf5a891c5580542811b906616f0c15d0dd0fc
-# cd ..
-
-# cargo +nightly-2023-06-20 build --manifest-path stc/crates/stc/Cargo.toml --no-default-features --locked
-
-# ./stc/target/release/stc --help
-
-# echo "::endgroup::"
+echo "::group::Build checkers"
 
 echo "::group::Build Ezno (and demo.tsx)"
+
 rustc --version
 rustup toolchain install stable
 
-# Main
-git clone https://github.com/kaleidawave/ezno.git ezno -b main
+# ezno main
+if [ -d "ezno" ]; then
+    cd ezno
+    git init
+    git remote add origin https://github.com/kaleidawave/ezno.git
+    git fetch
+    git checkout origin/general-fixes -ft
+    cd ..
+else
+    git clone https://github.com/kaleidawave/ezno.git ezno -b general-fixes
+fi
 
 cargo build --manifest-path ezno/Cargo.toml --release --bin ezno
+./ezno/target/release/ezno info
+
+date
 
 # and new parser
-git clone https://github.com/kaleidawave/ezno.git ezno-next -b merge-lexer
-cargo build --manifest-path ezno-next/Cargo.toml --release --bin ezno
-
+# git clone https://github.com/kaleidawave/ezno.git ezno-next -b merge-lexer
+# cargo build --manifest-path ezno-next/Cargo.toml --release --bin ezno
 # cargo install --path ezno
+# ./ezno/target/release/ezno-next info
 
-./ezno/target/release/ezno info
-./ezno/target/release/ezno-next info
+echo "::endgroup::"
+
+# ---
+
+echo "::group::Get (old) TSC"
+
+npm i -g typescript
+
+echo "::endgroup"
+
+# ---
+
+echo "::group::Get (new) TSC"
+
+# new tsc
+if [ -d "tsc-go" ]; then
+    cd tsc-go
+    git init
+    git remote add origin https://github.com/microsoft/typescript-go.git
+    git fetch
+    git checkout origin/main -ft
+    cd ..
+else
+    git clone --recurse-submodules https://github.com/microsoft/typescript-go.git tsc-go
+fi
+
+cd tsc-go
+git submodule update --init --recursive
+npm i
+npm run build
+cd ..
+
+echo "::endgroup"
+
+# ---
+
+echo "::endgroup::"
+
+# ---
 
 echo "::group::Build demo files"
+
 cargo run --manifest-path ezno/Cargo.toml \
-    -p ezno-parser --example code_blocks_to_script ./ezno/checker/specification/specification.md \
-    --comment-headers --out ./demo.tsx
+    -p ezno-checker-specification \
+    --example amalgamate ezno/checker/specification/specification.md \
+    --comment-headers \
+    --repeat 1 \
+    --out ./demo.ts
 
-cp ./demo.tsx $ARTIFACTS_FOLDER
+cp ./demo.ts $ARTIFACTS_FOLDER
+
+cargo run --manifest-path ezno/Cargo.toml \
+    -p ezno-checker-specification \
+    --example amalgamate ezno/checker/specification/specification.md \
+    --comment-headers \
+    --repeat 40 \
+    --out ./large.ts
+
+cp ./large.ts $ARTIFACTS_FOLDER
+
+# # Simple
+# echo "const x: string = 4;" >> simple.tsx
+
+# # Large
+# for i in {1..10}; do
+#     cat ./demo.tsx >> ./large.tsx
+# done
+
+# echo "interface Array {}; interface Boolean {}; interface Function {}; interface IArguments {}; interface Number {}; interface Object {}; interface RegExp {}; interface String {}" > overrides.d.ts
 
 echo "::endgroup::"
-
-echo "::endgroup::"
-
-echo "::group::Get TSC"
-
-npm install -g typescript
-
-echo "::endgroup"
-
-echo "::endgroup"
