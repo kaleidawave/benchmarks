a833cba033a83cdca36e65c0a689626723cee663
Ben <kaleidawave@gmail.com>
2025-09-24 13:11:49 +0100

    Add shell wrapping to GITHUB_STEP_SUMMARY

--- benchmarks/ts-type-checkers/run.sh
+++ benchmarks/ts-type-checkers/run.sh
@@ -23,9 +23,17 @@ echo "typescript (tsgo) demo10"
 ./node_modules/@typescript/native-preview-linux-x64/lib/tsgo --noEmit --jsx preserve --skipLibCheck --diagnostics demo10.tsx || true
 
 echo "comparison demo"
+
+echo "\`\`\`shell\n" >> "$GITHUB_STEP_SUMMARY"
         
 hyperfine -N -i './ezno check demo.tsx' './node_modules/@typescript/native-preview-linux-x64/lib/tsgo --noEmit --jsx preserve --skipLibCheck demo.tsx' 'node ./node_modules/typescript/lib/_tsc.js --noEmit --jsx preserve --skipLibCheck demo.tsx' >> "$GITHUB_STEP_SUMMARY"
 
+echo "\n\`\`\`\n\n" >> "$GITHUB_STEP_SUMMARY"
+
 echo "comparison demo10"
 
+echo "\`\`\`shell\n" >> "$GITHUB_STEP_SUMMARY"
+
 hyperfine -N -i './ezno check demo10.tsx' './node_modules/@typescript/native-preview-linux-x64/lib/tsgo --noEmit --jsx preserve --skipLibCheck demo10.tsx' 'node ./node_modules/typescript/lib/_tsc.js --noEmit --jsx preserve --skipLibCheck demo10.tsx' >> "$GITHUB_STEP_SUMMARY"
+
+echo "\n\`\`\`" >> "$GITHUB_STEP_SUMMARY"
