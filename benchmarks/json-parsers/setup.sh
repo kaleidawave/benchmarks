2f81ed53d65bbf2804f0ede3d3c6846f150b639f
Ben <kaleidawave@gmail.com>
2026-02-04 18:14:52 +0000

    Improve file paths

--- benchmarks/json-parsers/setup.sh
+++ benchmarks/json-parsers/setup.sh
@@ -1,12 +1,13 @@
-gh repo clone kaleidawave/depict
-cd depict 
+gh repo clone kaleidawave/depict depict-tool
+cd depict-tool
 git checkout cli-improvements
 cargo build
-mv target/debug/depict ~
-cd ~
+mv target/debug/depict ..
+cd ..
 
 gh repo clone kaleidawave/simple-json-parser
 cd simple-json-parser
 git checkout improvements
 cargo b --release --example parse
-mv target/release/examples/parse ~
+mv target/release/examples/parse ..
+cd ..
