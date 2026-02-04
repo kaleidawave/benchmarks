4cd083c42a4a6fb77562bad7d2ccca6e0d86e4f5
Ben <kaleidawave@gmail.com>
2026-02-04 18:06:24 +0000

    cannot spell

--- benchmarks/json-parsers/setup.sh
+++ benchmarks/json-parsers/setup.sh
@@ -1,9 +1,9 @@
-gh repo clone kaleidwave/depict
+gh repo clone kaleidawave/depict
 cd depict && git checkout cli-improvements && cd ..
 cargo install --path depict
 depict install
 
-gh repo clone kaleidwave/simple-json-parser .
+gh repo clone kaleidawave/simple-json-parser .
 git checkout improvements
 git status
 cargo b --release --example parse
